e77c24b37e12ff3093c8c7e24c83f84f1d2893f2
Caleb975 <cpromanru@esofa.edu.pe>
2026-09-10 12:36:52 -0500

    formativa4

--- archivo3.py
+++
@@ -1,5 +0,0 @@
-# GRADO Y NSA: AL3 FAP 613659
-# APELLIDOS Y NOMBRES: ROMAN RUIZ CALEB PABLO
-# ESPECIALIDAD Y AÑO: INFORMATICA - III AÑO
-
-print("Evaluacion practica ejecutada correctamente en la rama 3")
